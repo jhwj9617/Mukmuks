bbaec21a5fec23032f7441d95bdad9e42919aff4
Joon Ho <jhwj9617@live.com>
2015-03-24 22:57:44 -0700

    settings wsgi changes

--- mukmuks/mukmuks/settings.py
+++ mukmuks/mukmuks/settings.py
@@ -67,6 +67,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES[‘default’] = dj_database_url.config()
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -86,3 +90,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, ‘static’),
+)
+
+SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
