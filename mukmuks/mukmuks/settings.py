3d762293aeaebea8785d8172815103a2fc979069
Joon Ho <jhwj9617@live.com>
2015-03-26 21:01:42 -0700

    psql set

--- mukmuks/mukmuks/settings.py
+++ mukmuks/mukmuks/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'mukmuks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mylocaldb',
+        'USER': 'Joon',
+        'PASSWORD': '022597733',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
